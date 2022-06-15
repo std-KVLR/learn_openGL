15379f554f3faa289e1d3f42a305e33f85e14f8c
Sklyarevsky Sergey <ss13uRn1nG@gmail.com>
2022-06-15 16:25:07 +0300

    Lesson 2
    Learn shaders

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,8 +1,94 @@
 #include <iostream>
-
-
+#include <fstream>
+#include <string>
+#include <utility>
 #include <glad.h>
 #include <GLFW/glfw3.h>
+
+namespace myOpenGL
+{
+
+struct GlError
+{
+    static constexpr int maxLogSize = 512;
+    int successful;
+    char info[maxLogSize];
+};
+
+class Shader
+{
+public:
+    class ShaderCompilationError {
+    public:
+        const std::string what;
+        ShaderCompilationError(const char* err)
+            :what(err) {}
+
+    };
+
+protected:
+
+    void compile_process(const char* fileName, unsigned glCompileStatus)
+    {
+        std::ifstream shaderFile(fileName, std::ios::binary);
+
+        if(shaderFile.is_open())
+        {
+            std::string shaderSource;
+            char c;
+            while(shaderFile >> c) {
+                shaderSource.push_back(c);
+            }
+            const char* buffer = shaderSource.c_str();
+            m_shader = glCreateShader(glCompileStatus);
+            glShaderSource(m_shader, 1, &buffer , NULL);
+            glCompileShader(m_shader);
+        }
+        shaderFile.close();
+    }
+
+    void compile_info()
+    {
+        GlError err;
+
+        glGetShaderiv(m_shader, GL_COMPILE_STATUS, &(err.successful));
+
+        if(!err.successful){
+            glGetShaderInfoLog(m_shader, 512, NULL, err.info);
+            throw ShaderCompilationError(err.info);
+        }
+
+    }
+    unsigned m_shader;
+
+public:
+    virtual void compile(const char* fileName) = 0;
+    virtual unsigned get() { return m_shader; }
+};
+
+
+class VertexShader final: public Shader
+{
+public:
+    VertexShader():Shader(){}
+
+    void compile(const char* fileName)
+    {
+        compile_process(fileName, GL_VERTEX_SHADER);
+        compile_info();
+    }
+};
+
+class FragmentShader final: public Shader
+{
+public:
+    FragmentShader():Shader(){}
+
+    void compile(const char* fileName)
+    {
+        compile_process(fileName, GL_FRAGMENT_SHADER);
+    }
+};
 
 void processInput(GLFWwindow* window)
 {
@@ -55,19 +141,58 @@ GLFWwindow* OpenGlSetup()
     return window;
 };
 
+}
+
+
 int main()
 {
-    auto glWindow = OpenGlSetup();
+    auto glWindow = myOpenGL::OpenGlSetup();
 
     if(glWindow != nullptr){
+        float vertices[] =
+        {
+           -0.5f, -0.5f, 0.0f,
+            0.5F, -0.5f, 0.0f,
+            0.0f,  0.5f, 0.0f
+        };
+        unsigned VBO;
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        unsigned int shaderProgram;
+        shaderProgram = glCreateProgram();
+        myOpenGL::VertexShader vxsh;
+        myOpenGL::FragmentShader fgsh;
+        try
+        {
+            vxsh.compile("../../shaders/vertex_shader1.glsl");
+            fgsh.compile("../../shaders/fragment_shader1.glsl");
 
-        while(!glfwWindowShouldClose(glWindow)) {
-            glClear(GL_COLOR_BUFFER_BIT);
-            processInput(glWindow);
-            glfwSwapBuffers(glWindow);
-            glfwPollEvents();
         }
-   }
+        catch(myOpenGL::Shader::ShaderCompilationError& err)
+        {
+            std::cerr << err.what << std::endl;
+        }
+
+        glAttachShader(shaderProgram, vxsh.get());
+        glAttachShader(shaderProgram, fgsh.get());
+        glLinkProgram(shaderProgram);
+        myOpenGL::GlError err;
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &err.successful);
+        if(!err.successful){
+            glGetProgramInfoLog(shaderProgram, err.maxLogSize, NULL, err.info);
+            std::cerr << err.info << std::endl;
+        }
+        glUseProgram(shaderProgram);
+        glDeleteShader(vxsh.get());
+        glDeleteShader(fgsh.get());
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(0));
+        glEnableVertexAttribArray(0);
+
+    }
+
    glfwTerminate();
    return 0;
+
 }
